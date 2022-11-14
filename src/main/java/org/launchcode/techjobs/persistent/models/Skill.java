08d012bee5f8b8924fffbb52946b1ce7eed5b32e
jfine <jonfine@gmail.com>
2022-11-13 20:55:39 -0600

    Passed tests; uploading

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,36 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank(message = "Skill is required")
+    private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    public void skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
 }
